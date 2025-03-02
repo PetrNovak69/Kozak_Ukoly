414444958078b7b767e83f72ff7477ac50185923
PetrNovak69 <100471782+PetrNovak69@users.noreply.github.com>
2025-03-02 19:36:58 +0100

    Delete Dilatace directory

--- Dilatace/uloha1.py
+++
@@ -1,6 +0,0 @@
-# Inicializace image2d
-image2d = [[4 - max(abs(i - 4), abs(j - 4)) for j in range(9)] for i in range(9)]
-
-# Výstup matice v jednotlivých řádcích
-for row in image2d:
-    print(row)
